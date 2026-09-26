3b5f6f193b238e74bc89369dfed7711f31416b50
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2026-09-26 13:02:57 +0530

    robots.ts added for the Google Console System

---
+++ frontend/src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: "*",
+            allow: "/",
+            disallow: ["/admin", "/api"],
+        },
+        sitemap: "https://trustline.com/sitemap.xml",
+
+        };
+    }
